02fca49d2c93beea904eaf8b0688656180ed7415
Turki BinTalib <turki.fcit@gmail.com>
2018-02-09 01:09:56 +0300

    Add files via upload

---
+++ test/testapps.js
@@ -0,0 +1,39 @@
+var assert = require('chai').assert;
+//var haraj = require("../haraj");
+//var opensooq = require("../opensooq");
+//var sooqmzad = require("../sooqmzad");
+var info_gmap = require("../info-gmap");
+
+describe("Test Apps", function(){
+
+    //Error due to Casperjs
+    /*describe("Haraj", function(){
+        it("No test :(", function(){
+
+        })
+    })*/
+
+    /*describe("OpenSooq", function(){
+        it("No test :(", function(){
+        })
+    })*/
+
+    /*describe("SooqMzad", function(){
+        it("No test :(", function(){
+        })
+    })*/
+
+    describe("Info_GMap", function(){
+
+        it("Keyword is array", function(){
+            var result = info_gmap.keyword;
+            assert.isArray(result, 'what Keyword of place do we want?');
+        })
+
+        it("Keyword has 8 variables", function(){
+            var result = info_gmap.keyword;
+            assert.lengthOf(result,8,'Keyword has length of 8');
+        })
+    })
+
+})
