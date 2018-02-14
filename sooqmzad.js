31d83dd3b371aa1ab8576f7e297923708466ea00
Turki BinTalib <turki.fcit@gmail.com>
2018-02-14 16:27:14 +0300

    Add files via upload

--- sooqmzad.js
+++ sooqmzad.js
@@ -4,6 +4,41 @@ var fs = require('fs');
 
 var result = [];
 
+function GetDescription (name,link,date,CityName,SectionName){
+    var description;
+    request(link, function (error, response, body) {
+        if (error) {
+            console.log("error");
+            return;
+        }
+        var $ = cheerio.load(body)
+        var ourDiv = $('div.panel.panel-default.panel-DA');
+        ourDiv.each(function(index) {
+            var divdata = $(this).find('div.panel-body')
+            divdata.each(function(index) {
+                description = $('div.text.ckeditor-code', this).text().trim();
+
+                console.log("Name: "+name);
+                console.log("Link: "+link);
+                console.log("Date: "+date);
+                console.log("Description: "+description);
+                console.log("City: "+CityName);
+                console.log("Section: "+ SectionName);
+                console.log('****************************************')
+                result.push({
+                    name: name,
+                    link: link,
+                    price: null,
+                    date: date,
+                    description: description,
+                    city: CityName,
+                    section: SectionName
+                });
+            })
+        })
+    })
+}
+
 function CollectData (url,CityName,SectionName){
     for(var NumPage =1;NumPage<=5;NumPage++){
         request(url+NumPage, function (error, response, body) {
@@ -28,19 +63,9 @@ function CollectData (url,CityName,SectionName){
                     var link = $(this).find('a.ads-name')
                     link.each(function(index) {
                         link=this.attribs.href;
-                        console.log("Name: "+name);
-                        console.log("Link: "+link);
-                        console.log("Date: "+date[1]);
-                        console.log("City: "+CityName);
-                        console.log("Section: "+ SectionName);
-                        console.log('****************************************')
-                        result.push({
-                            name: name,
-                            link: link,
-                            date: date[1],
-                            city: CityName,
-                            section: SectionName
-                        });
+
+                        GetDescription(name,link,date,CityName,SectionName);
+
                     })
                 })
             })
