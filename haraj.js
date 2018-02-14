778752f44d1e9d66c064885bb6757ffbb4873062
Turki BinTalib <turki.fcit@gmail.com>
2018-02-14 15:06:41 +0300

    Add files via upload

--- haraj.js
+++ haraj.js
@@ -14,6 +14,7 @@ var casper = require('casper').create({
 var itemsList = {};
 var result = [];
 var currentSuite = 0;
+var ToGetDate=1;
 
 function getNames() {
     var names = document.querySelectorAll('div.adsx div.adxTitle a');
@@ -29,29 +30,41 @@ function getLinks() {
     });
 }
 
+function getDate() {
+    var Dates = document.querySelectorAll('div.adsx div.adxExtraInfo div.adxExtraInfoPart');
+    
+    return _.map(Dates, function (e) {
+        return e.innerText;
+    });
+}
+
 function click(){
     //this.scrollToBottom();
     casper.click(".pagination li a");
     casper.wait(5000, function () {
     });
-    casper.click(".pagination li a");
 }
 
 function CollectData(CityName,SectionName){
     itemsList.names = casper.evaluate(getNames);
     itemsList.links = casper.evaluate(getLinks);
+    itemsList.date = casper.evaluate(getDate);
     for (i = 0; i < itemsList.names.length; i++) {
         result.push({
         name: itemsList.names[i],
         link: itemsList.links[i],
         city: CityName,
+        date:itemsList.date[ToGetDate],
         section: SectionName
         });
     console.log("Name: "+itemsList.names[i]);
     console.log("Link: "+itemsList.links[i]);
+    console.log("Date: "+itemsList.date[ToGetDate]);
     console.log("City: "+CityName);
     console.log("Section: "+ SectionName);
     console.log('****************************************')
+
+    ToGetDate+=4;
     }
 }
 
@@ -69,6 +82,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 2 - Haraj Jeddah Cars");
         this.start('https://haraj.com.sa/tags/%D8%AC%D8%AF%D9%87_%D8%AD%D8%B1%D8%A7%D8%AC%20%D8%A7%D9%84%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA');
 
@@ -81,6 +96,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 3 - Haraj Jeddah Furniture");
         this.start('https://haraj.com.sa/tags/%D8%AC%D8%AF%D9%87_%D8%A7%D8%AB%D8%A7%D8%AB');
 
@@ -93,6 +110,8 @@ var suites = [
         });
     }, 
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 4 - Haraj Mecca Electronic Devices");
         this.start('https://haraj.com.sa/tags/%D9%85%D9%83%D9%87_%D8%AD%D8%B1%D8%A7%D8%AC%20%D8%A7%D9%84%D8%A3%D8%AC%D9%87%D8%B2%D8%A9');
 
@@ -105,6 +124,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 5 - Haraj Mecca Cars");
         this.start('https://haraj.com.sa/tags/%D9%85%D9%83%D9%87_%D8%AD%D8%B1%D8%A7%D8%AC%20%D8%A7%D9%84%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA');
 
@@ -117,6 +138,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 6 - Haraj Mecca Furniture");
         this.start('https://haraj.com.sa/tags/%D9%85%D9%83%D9%87_%D8%A7%D8%AB%D8%A7%D8%AB');
 
@@ -129,6 +152,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 7 - Haraj Riyadh Electronic Devices");
         this.start('https://haraj.com.sa/tags/%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6_%D8%AD%D8%B1%D8%A7%D8%AC%20%D8%A7%D9%84%D8%A3%D8%AC%D9%87%D8%B2%D8%A9');
 
@@ -141,6 +166,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 8 - Haraj Riyadh Cars");
         this.start('https://haraj.com.sa/tags/%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6_%D8%AD%D8%B1%D8%A7%D8%AC%20%D8%A7%D9%84%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA');
 
@@ -153,6 +180,8 @@ var suites = [
         });
     },
     function() {
+        ToGetDate=1;
+
         this.echo("Suite 9 - Haraj Riyadh Furniture");
         this.start('https://haraj.com.sa/tags/%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6_%D8%A7%D8%AB%D8%A7%D8%AB');
 
