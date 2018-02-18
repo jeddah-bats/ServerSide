14c0cad7a993fddc96f17bc85a5dbebb426dfa40
Turki BinTalib <turki.fcit@gmail.com>
2018-02-18 17:48:50 +0300

    Add files via upload

--- info-gmap.js
+++ info-gmap.js
@@ -1,6 +1,8 @@
 const API_KEY = process.env.API_KEY;
+module.exports.API_KEY = process.env.API_KEY;
 const request = require('request');
 var fs = require('fs');
+var result = [];
 
 var Info=[
     // city   latitude  longitude 
@@ -9,23 +11,43 @@ var Info=[
     ['Riyadh',24.713552,46.675296]
 ]
 
+module.exports.keyword = ['car','electronics','devices','furniture','video games','fashion','clothing','hardware'];
 var keyword = ['car','electronics','devices','furniture','video games','fashion','clothing','hardware'];
 //var keyword = "car,electronics,devices,furniture,video games,fashion,clothing,hardware";
 
-for (var j =0;j<Info.length;j++){
-    for(var i =0;i<keyword.length;i++){
-        var location = Info[j][1] + ',' + Info[j][2];
-        var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
-        + location + '&rankby=distance' + '&keyword=' + keyword[i] + '&key=' + API_KEY;
-        console.log(url);
-        request(url, { json: true }, function (err, res, body) {
+function GetPlaces (url,city,section){
+    request(url, { json: true }, function (err, res, body) {
         if (err) {
             console.log(err);
             return;
         }
-        console.log(body);
-        console.log("==========================================================================================================================================================")
-        //fs.write('./'+Info[j][0]+'.json', JSON.stringify(body), 'w')
+
+        PushPlaces (body,url,city,section);
+
         });
+}
+
+function PushPlaces (body,url,city,section){
+    for(var i = 0;i<body.results.length;i++){
+        result.push({
+            name: body.results[i].name,
+            url: url,
+            city: city,
+            section: section,
+            type:"shop"
+        });
+
+        fs.writeFileSync('./ResultsInfo-GMap.json', JSON.stringify(result));
+    }
+}
+
+for (var j =0;j<Info.length;j++){
+    for(var i =0;i<keyword.length;i++){
+        var location = Info[j][1] + ',' + Info[j][2];
+        var city = Info[j][0];
+        var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+        + location + '&rankby=distance' + '&keyword=' + keyword[i] + '&key=' + API_KEY;
+        
+        GetPlaces(url,city,keyword[i]);
     }
 }
--- mongodb.js
+++ mongodb.js
@@ -12,10 +12,13 @@ MongoClient.connect(url, function(err, db) {
   var opensooq = JSON.parse(data2);
 
   var data3 = fs.readFileSync("ResultsSooqMzad.json");
-  var sooqmzad = JSON.parse(data3);
+  var sooqmzad = JSON.parse(data3); 
+
+  var data4 = fs.readFileSync("ResultsInfo-GMap.json");
+  var info_gmap = JSON.parse(data4); 
 
   for(var i = 0 ; i < Object.keys(opensooq).length; i++){
-    dbo.collection("data").insertOne(
+    dbo.collection("Products").insertOne(
       {
         name: opensooq[i].name,
         link: opensooq[i].link,
@@ -30,7 +33,7 @@ MongoClient.connect(url, function(err, db) {
   }
 
   for(var i = 0 ; i < Object.keys(haraj).length; i++){
-    dbo.collection("data").insertOne(
+    dbo.collection("Products").insertOne(
       {
         name: haraj[i].name,
         link: haraj[i].link,
@@ -45,7 +48,7 @@ MongoClient.connect(url, function(err, db) {
   }
 
   for(var i = 0 ; i < Object.keys(sooqmzad).length; i++){
-    dbo.collection("data").insertOne(
+    dbo.collection("Products").insertOne(
       {
         name: sooqmzad[i].name,
         link: sooqmzad[i].link,
@@ -59,9 +62,24 @@ MongoClient.connect(url, function(err, db) {
     });
   }
 
+  for(var i = 0 ; i < Object.keys(info_gmap).length; i++){
+    dbo.collection("Places").insertOne(
+      {
+        name: info_gmap[i].name,
+        url: info_gmap[i].url,
+        city: info_gmap[i].city,
+        section: info_gmap[i].section, 
+        type: info_gmap[i].type
+        }
+      , function(err, res) {
+      if (err) throw err;
+    });
+  }
+
   console.log("ResultsOpenSooq.json inserted"); 
   console.log("d-ResultsHaraj.json inserted"); 
-  console.log("ResultsSooqMzad.json inserted"); 
+  console.log("ResultsSooqMzad.json inserted");
+  console.log("ResultsInfo-GMap.json inserted"); 
 
   db.close();
 });
